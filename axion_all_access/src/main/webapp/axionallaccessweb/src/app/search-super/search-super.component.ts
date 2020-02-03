4d042e9367db537384828d4f7e9885d27631e0ce
CaiCoding2 <caicoding2@gmail.com>
2020-02-02 18:01:09 -0600

    search superbeing in employee side

--- axion_all_access/src/main/webapp/axionallaccessweb/src/app/search-super/search-super.component.ts
+++ axion_all_access/src/main/webapp/axionallaccessweb/src/app/search-super/search-super.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Superbeing } from '../superbeing.class';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { UserService } from '../user.service';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-search-super',
@@ -8,11 +11,53 @@ import { Superbeing } from '../superbeing.class';
 })
 export class SearchSuperComponent implements OnInit {
 
-  private super:Superbeing;
+  private supers:Superbeing[];
+  beingForm:FormGroup;
+  submitted = false;
+  loading = false;
+  id:number;
+  closeResult: string;
   
-  constructor() { }
+  constructor(private service:UserService, private formBuilder:FormBuilder,private modalService:NgbModal) {
+    
+   }
 
   ngOnInit() {
+    this.beingForm = this.formBuilder.group ({
+      superName: ['']
+    })
+    
+  }
+  get f() { return this.beingForm.controls }
+
+  onSubmit(){
+    this.submitted =true;
+    if(this.beingForm.invalid){
+      return;
+    }else{
+      this.service.getBySuperName(this.f.superName.value).subscribe(data=>{
+        this.supers = data;
+        this.loading = true;
+      });
+    }
   }
 
+  open(content,id) {
+    this.id=id;
+    this.modalService.open(content,{size:'xl',centered:true}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
